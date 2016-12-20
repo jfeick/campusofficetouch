02457d268d887ab4a64479cb3dc82212a76a2a6f
Jan Frederick Eick <jan-frederick.eick@uni-weimar.de>
2016-12-20 22:57:58 +0100

    added eventcal spider

---
+++ sort_cal.py
@@ -0,0 +1,11 @@
+import json
+from time import strptime
+
+cal_data = None
+with open('eventcal.json', 'r+') as json_file:
+    cal_data = json.load(json_file)
+
+    cal_data.sort(key=lambda x: (strptime(x['month'], '%b').tm_mon, x['day_of_month']))
+    json_file.seek(0)
+    json.dump(cal_data, json_file)
+    json_file.truncate()
--- uni_weimar/spiders/eventcal.py
+++ uni_weimar/spiders/eventcal.py
@@ -1,25 +1,83 @@
 import scrapy
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy.utils.misc import md5sum
+from io import BytesIO
+from PIL import Image, ImageOps
+import os
+
 import logging
+import json
+
+#from uni_weimar.items import JournalItem
+
 import re
+import lxml
+import logging
 
-from uni_weimar.items import ShowCaseItem, ShowCaseImage, ShowCaseVideo
 
-class ShowcaseSpider(scrapy.Spider):
-    name = "showcase"
+def clean(dirty_string):
+    res = dirty_string.replace('\t', '')
+    res = res.replace('\n', '')
+    res = res.replace('\r', '')
+    return res.strip()
+
+class CalendarItem(scrapy.Item):
+    title = scrapy.Field()
+    weekday = scrapy.Field()
+    day_of_month = scrapy.Field()
+    month = scrapy.Field()
+    subtitle = scrapy.Field()
+    maininfo = scrapy.Field()
+    bodytext = scrapy.Field()
+
+class CalendarSpider(scrapy.Spider):
+    name = "eventcal"
     allowed_domains = ["uni-weimar.de"]
     start_urls = [
-    "http://www.uni-weimar.de/de/universitaet/aktuell/veranstaltungskalender/"
-    ]
+    "http://www.uni-weimar.de/de/universitaet/aktuell/veranstaltungskalender/"   ]
 
     def parse(self, response):
-        listview = response.css("div.listview.listview-content.listview-201.listview-content-201")
-        # we are selecting links from a svg namespace. selectors extract xlink:href attribute
-        records = listview.css("div.record01")
-        for record in records:
-            link = record.css("div.text01 > h2 > a")
-            href = link.xpath("@href")
-            url = response.urljoin(href.extract()[0])
-            yield scrapy.Request(url, callback=self.parse_eventcal_item)
 
-    def parse_eventcal_item(self, response):
-        pass
+        list_items = response.css('.record01')
+        for list_item in list_items:
+            date = list_item.css('.my_datesheet')
+            weekday = date.css('.weekday').xpath('a/text()').extract()[0]
+            day_of_month = date.css('.day_of_month').xpath('a/text()').extract()[0]
+            month = date.css('.month').xpath('a/text()').extract()[0]
+            text = list_item.css('.text01')
+            title = text.css('h2').xpath('a/text()').extract()[0]
+            href = text.css('h2').xpath('a/@href')
+            
+            subtitle = text.css('.cal_subtitle').xpath('text()').extract()
+            subtitle = "".join(subtitle)
+
+            maininfo = text.css('.cal_maininfo').xpath('*').extract()
+            maininfo = "".join(maininfo)
+
+            url = response.urljoin(href.extract()[0])
+
+            item = CalendarItem()
+            item['weekday'] = clean(weekday)
+            item['day_of_month'] = clean(day_of_month)
+            item['month'] = clean(month)
+            item['subtitle'] = clean(subtitle)
+            item['title'] = clean(title)
+            item['maininfo'] = clean(maininfo);
+
+            self.logger.info("Yielding item")
+
+            yield scrapy.Request(url, callback=self.parse_calendar_item, meta={'item': item})
+            #yield item
+
+    def parse_calendar_item(self, response):
+        item = response.meta['item']
+        bodytext = response.css('.cal_bodytext').xpath('*').extract()
+        bodytext = "".join(bodytext)
+
+        # strip links with regex
+        p = re.compile(r"<\/?a(?:(?= )[^>]*)?>")
+        bodytext = re.sub(p, "", bodytext)
+
+        item['bodytext'] = bodytext
+
+        yield item
